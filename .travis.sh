1941bb9247eda14b27fdc04b6cb9f52701f038db
int128 <int128@gmail.com>
2015-03-15 02:08:15 +0900

    Add hook to create PR for user repo

--- .travis.sh
+++ .travis.sh
@@ -15,6 +15,29 @@ function update-gradle-template () {
   git push origin --delete "$TRAVIS_BRANCH"
 }
 
+function update-gradle-of-user-repository () {
+  local REPO="${TRAVIS_BRANCH#*-}"
+  local TEMPLATE_DIR="$PWD"
+  local GRADLE_VERSION="$(cat gradle-version)"
+  test "$GRADLE_VERSION"
+
+  git clone "https://github.com/gradleupdate/${REPO}.git" "$HOME/$REPO"
+  cd "$HOME/$REPO"
+  git checkout -b "gradle-$GRADLE_VERSION"
+  mkdir -p gradle/wrapper
+  cp -a "$TEMPLATE_DIR/gradle/wrapper" ./gradle
+  cp -a "$TEMPLATE_DIR/gradlew" .
+  cp -a "$TEMPLATE_DIR/gradlew.bat" .
+  sed -i -e "s,gradleVersion *= *['\"][0-9a-z\.\-]\+['\"],gradleVersion = '$GRADLE_VERSION',g" build.gradle
+  git add gradle/wrapper gradlew gradlew.bat build.gradle
+  git commit -m "Gradle $GRADLE_VERSION"
+  git push origin "gradle-$GRADLE_VERSION"
+
+  cd "$TEMPLATE_DIR"
+  git push origin --delete "$TRAVIS_BRANCH"
+}
+
 case "$TRAVIS_BRANCH" in
   build-*)  update-gradle-template;;
+  pr-*)     update-gradle-of-user-repository;;
 esac
